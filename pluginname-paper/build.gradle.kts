f8d968b7d12334098eb213372e98ef9a4c694b0a
Kevin <kevin@fritsch.vip>
2022-07-09 18:11:40 +0200

    added 1.19 template

---
+++ pluginname-paper/build.gradle.kts
@@ -0,0 +1,33 @@
+import org.apache.tools.ant.filters.ReplaceTokens
+
+plugins {
+    java
+    id("com.github.johnrengelman.shadow")
+}
+
+repositories {
+    maven {
+        name = "papermc-repo"
+        url = uri("https://repo.papermc.io/repository/maven-public/")
+    }
+}
+
+dependencies {
+    implementation(project(":pluginname-common"))
+    compileOnly("io.papermc.paper:paper-api:1.19-R0.1-SNAPSHOT")
+}
+
+tasks {
+    // If you open resources/plugins.yml you will see "@version@" as the version this code replaces this
+    processResources {
+        from(sourceSets["main"].resources) {
+            val tokens = mapOf("version" to version)
+            filter(ReplaceTokens::class, mapOf("tokens" to tokens))
+            duplicatesStrategy = DuplicatesStrategy.INCLUDE
+        }
+    }
+
+    build {
+        dependsOn(shadowJar)
+    }
+}
